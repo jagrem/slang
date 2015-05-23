93714210c2914a3dd419f199a40ab044994e8339
jagrem <jagremc@gmail.com>
2015-05-23 17:49:13 +1200

    Add a compilation unit type that represents a parse tree from a specific file.

---
+++ slang/Compilation/CompilationUnit.cs
@@ -0,0 +1,18 @@
+﻿using slang.AST;
+
+namespace slang.Compilation
+{
+    public class CompilationUnit
+    {
+        public CompilationUnit (string sourceFilename, ProgramNode root)
+        {
+            SourceFile = sourceFilename;
+            Root = root;
+        }
+
+        public string SourceFile { get; set; }
+
+        public ProgramNode Root { get; set; }
+    }
+}
+
