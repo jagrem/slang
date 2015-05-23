ff774510037c6c8908a590623260e59c8617075f
jagrem <jagremc@gmail.com>
2015-05-23 17:48:33 +1200

    Add a compilation root as a container for individual compilation units created by parsing each file.

---
+++ slang/Compilation/CompilationRoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace slang.Compilation
+{
+    public class CompilationRoot
+    {
+        public CompilationRoot (CompilationMetadata metadata)
+        {
+            CompilationUnits = new List<CompilationUnit> ();
+            Metadata = metadata;
+        }
+
+        public List<CompilationUnit> CompilationUnits { get; private set; }
+
+        public CompilationMetadata Metadata { get; private set; }
+    }
+}
+
