aca98426ca06a65ca49166bd4f0682e98200ca12
jagrem <jagremc@gmail.com>
2015-12-13 10:02:57 +1300

    Add identifiers and tests
    Add an identifier token and a test fixture with one failing test for identifiers.

--- slang.Tests/Lexing/IdentifierTests.cs
+++ slang.Tests/Lexing/IdentifierTests.cs
@@ -1,9 +1,25 @@
-﻿using System;
+﻿using NUnit.Framework;
+using slang.Lexing;
+using System.Linq;
+using FluentAssertions;
+using System.Collections.Generic;
+using slang.Lexing.Tokens;
 
-namespace slang.Tests.Parsing.Literals
+namespace slang.Tests.Lexing
 {
+    [TestFixture]
     public class IdentifierTests
     {
+        [TestCaseSource("GetIdentifiers")]
+        public void Given_an_identifier_as_a_string_When_parsed_Then_an_identifier_token_is_returned(string input)
+        {
+            var result = Lexer.Analyze (input).ToArray ();
+            result.ElementAt (1)
+                .Should().BeOfType<Identifier> ()
+                .Which.As<Identifier>()
+                .Value.Should ().Be (input);
+        }
+
         //identifier:
         //    available-identifier
         //    @   identifier-or-keyword
@@ -24,22 +40,24 @@ namespace slang.Tests.Parsing.Literals
         //    combining-character
         //    formatting-character
         //letter-character:
-        //    A Unicode character of classes Lu, Ll, Lt, Lm, Lo, or Nl 
+        //    A Unicode character of classes Lu, Ll, Lt, Lm, Lo, or Nl
         //    A unicode-escape-sequence representing a character of classes Lu, Ll, Lt, Lm, Lo, or Nl
         //    combining-character:
-        //    A Unicode character of classes Mn or Mc 
+        //    A Unicode character of classes Mn or Mc
         //    A unicode-escape-sequence representing a character of classes Mn or Mc
         //decimal-digit-character:
-        //    A Unicode character of the class Nd 
+        //    A Unicode character of the class Nd
         //    A unicode-escape-sequence representing a character of the class Nd
         //connecting-character:
-        //    A Unicode character of the class Pc 
+        //    A Unicode character of the class Pc
         //    A unicode-escape-sequence representing a character of the class Pc
         //formatting-character:
-        //    A Unicode character of the class Cf 
+        //    A Unicode character of the class Cf
         //    A unicode-escape-sequence representing a character of the class Cf
-        public void Given_an_identifer_as_a_string_When_parsed_Then_an_identifier_is_returned()
-        {}
+        static IEnumerable<TestCaseData> GetIdentifiers()
+        {
+            yield return new TestCaseData ("_");
+        }
     }
 }
 
---
+++ slang/Lexing/Transitions/Identifiers.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace slang.Lexing.Transitions
+{
+    public static class Identifiers
+    {
+        public static readonly Dictionary<State,Func<LexerState,LexerState>> Transitions = new Dictionary<State,Func<LexerState,LexerState>>
+        {
+        };
+    }
+}
+
