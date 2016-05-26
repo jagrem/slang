381d13086caeb2cd665121ea7b6109f922ebe8a1
jagrem <jagremc@gmail.com>
2016-05-26 22:16:46 +1200

    Add star rule

---
+++ slang/Lexing/Rules/Extensions/Star.cs
@@ -0,0 +1,15 @@
+﻿using slang.Lexing.Rules.Core;
+
+namespace slang.Lexing.Rules.Extensions
+{
+    public class Star : Rule
+    {
+        public Rule Value { get; set; }
+
+        public Star (Rule value)
+        {
+            Value = value;
+        }
+    }
+}
+
