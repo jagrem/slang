e68c5282f6238aaf65ee849088bf68599be4df07
jagrem <jagremc@gmail.com>
2016-07-11 18:15:42 +1200

    Add a model for transitions
    Add the token producer function to transition so it can be triggered when the transition is executed.

---
+++ slang/Lexing/Trees/Nodes/Transition.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using slang.Lexing.Tokens;
+namespace slang.Lexing.Trees.Nodes
+{
+    public class Transition
+    {
+        public Transition (Node target, Func<Token> tokenProducer = null)
+        {
+            Target = target;
+            TokenProducer = tokenProducer;
+        }
+
+        public Token GetToken()
+        {
+            if(TokenProducer != null)
+            {
+                return TokenProducer ();
+            }
+
+            return null;
+        }
+
+        public Func<Token> TokenProducer { get; private set; }
+
+        public void Returns (Func<Token> tokenProducer)
+        {
+            TokenProducer = tokenProducer;
+        }
+
+        public Node Target { get; }
+    }
+}
+
