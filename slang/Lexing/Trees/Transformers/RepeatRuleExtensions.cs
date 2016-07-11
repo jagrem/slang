ce78f3af70f35cf6ffb4b70275ec2c1c9937d398
jagrem <jagremc@gmail.com>
2016-07-11 18:13:02 +1200

    Transform repeat rules into a tree

---
+++ slang/Lexing/Trees/Transformers/RepeatRuleExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using slang.Lexing.Rules.Core;
+using slang.Lexing.Trees.Nodes;
+
+namespace slang.Lexing.Trees.Transformers
+{
+    public static class RepeatRuleExtensions
+    {
+        public static Tree Transform (this Repeat rule)
+        {
+            var tree = rule.Value.Transform ();
+            var transitions = tree.Root.Transitions;
+
+            tree.Leaves
+                .ToList ()
+                .ForEach (leaf => {
+                    transitions.ToList ().ForEach (transition => leaf.Transitions.Add (transition.Key, transition.Value));
+                    leaf.Transitions [Character.Any] = new Transition (null, rule.TokenCreator);
+                });
+
+            return tree;
+        }
+    }
+}
+
