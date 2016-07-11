7486dc2f84d5f57fefcab164ed126f61f34fba67
jagrem <jagremc@gmail.com>
2016-07-11 18:14:13 +1200

    Add new lexer
    This lexer uses rules to generate a state machine for lexing.

---
+++ slang/Lexing/Lexer2.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using slang.Lexing.Rules.Core;
+using slang.Lexing.Tokens;
+using slang.Lexing.Trees;
+using slang.Lexing.Trees.Nodes;
+using System.Linq;
+using System;
+
+namespace slang.Lexing
+{
+    public class Lexer2
+    {
+        readonly Node root;
+
+        public Lexer2 (Rule rule)
+        {
+            root = TreeBuilder.Build (rule).Root;
+        }
+
+        public IEnumerable<Token> Scan(string input)
+        {
+            return ScanInternal (input).Where (token => token != null);
+        }
+
+        IEnumerable<Token> ScanInternal(string input)
+        {
+            var current = root;
+
+            foreach (char c in input) {
+                var character = Character.FromChar (c);
+
+                if (current.Transitions.ContainsKey (character)) {
+                    var transition = current.Transitions [character];
+                    yield return transition.GetToken ();
+                    current = transition.Target;
+                } else {
+                    if(current.Transitions.ContainsKey (Character.Any)) {
+                        var transition = current.Transitions [Character.Any];
+                        yield return transition.GetToken ();
+
+                        if(transition.Target != null) {
+                            current = transition.Target;
+                        } else {
+                            current = root;
+                        }
+                    } else {
+                        current = root;
+                    }
+
+                    if (current.Transitions.ContainsKey (character)) {
+                        var transition = current.Transitions [character];
+                        yield return transition.GetToken ();
+                        current = transition.Target;
+                    }
+                }
+            }
+
+            // EOF
+            if (current.Transitions.ContainsKey (Character.Any)) yield return current.Transitions [Character.Any].GetToken ();
+        }
+    }
+}
+
