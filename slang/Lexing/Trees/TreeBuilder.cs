f1216d413d9c3f4418c9bea848788e5aa0675cf6
jagrem <jagremc@gmail.com>
2016-07-11 18:16:53 +1200

    Remove lexical prefix from tree builder

---
+++ slang/Lexing/Trees/TreeBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using slang.Lexing.Rules.Core;
+using slang.Lexing.Trees.Nodes;
+using slang.Lexing.Trees.Transformers;
+
+namespace slang.Lexing.Trees
+{
+    public static class TreeBuilder
+    {
+        public static Tree Build (Rule rule)
+        {
+            return rule.Transform ();
+        }
+
+        public static IEnumerable<Node> GetLeafNodes (Node node)
+        {
+            if(!node.Transitions.Any () 
+               || (
+                   node.Transitions.Count () == 1 
+                   && node.Transitions.ElementAt (0).Key == Character.Any
+                  )
+              ) 
+            {
+                return new [] { node };
+            }
+
+            return node
+                .Transitions
+                .SelectMany (n => {
+                    if (n.Value.Target == null) {
+                        return new Node [0];
+                    }
+
+                    if (!n.Value.Target.Transitions.Any ()) {
+                        return new [] { n.Value.Target };
+                    }
+
+                    return GetLeafNodes (n.Value.Target);
+                });
+        }
+    }
+}
+
