c67704e28c4b7388901ccba1d8b30b986bd9664c
jagrem <jagremc@gmail.com>
2016-07-11 18:19:14 +1200

    Add ability to return token from rule

--- slang/Lexing/Rules/Core/Rule.cs
+++ slang/Lexing/Rules/Core/Rule.cs
@@ -1,7 +1,11 @@
-﻿namespace slang.Lexing.Rules.Core
+﻿using slang.Lexing.Tokens;
+using System;
+namespace slang.Lexing.Rules.Core
 {
     public abstract class Rule
     {
+        public Func<Token> TokenCreator { get; set; }
+
         public static Rule operator | (Rule left, Rule right)
         {
             return new Or (left, right);
@@ -15,6 +19,12 @@
         public static implicit operator Rule (char value)
         {
             return new Constant (value);
+        }
+
+        public Rule Returns(Func<Token> tokenCreator = null)
+        {
+            TokenCreator = tokenCreator ?? new Func<Token>(() => Token.Empty);
+            return this;
         }
     }
 }
