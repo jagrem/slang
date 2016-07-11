93055b75bfb3d1b5f2b82251dad05bbecccd2926
jagrem <jagremc@gmail.com>
2016-07-11 18:18:01 +1200

    Add useful extensions for tree operations

---
+++ slang/Lexing/Trees/TreeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+namespace slang.Lexing.Trees
+{
+    public static class TreeExtensions
+    {
+        public static Tree AttachChild(this Tree parent, Tree child)
+        {
+            var parentLeaves = parent.Leaves.ToList ();
+            var childTransitions = child.Root.Transitions.ToList ();
+
+            parentLeaves.ForEach (parentLeaf =>
+                                  childTransitions.ForEach (childTransition =>
+                                          parentLeaf
+                                              .Transitions
+                                              .Add (childTransition.Key, childTransition.Value)));
+            return parent;
+        }
+
+        public static Tree Merge (this Tree left, Tree right)
+        {
+            var tree = new Tree ();
+            var leftTransitions = left.Root.Transitions.ToList() ;
+            var rightTransitions = right.Root.Transitions.ToList();
+            var transitions = leftTransitions.Concat (rightTransitions);
+            transitions.ToList ().ForEach (t => tree.Root.Transitions.Add (t.Key, t.Value));
+            return tree;
+        }
+    }
+}
+
