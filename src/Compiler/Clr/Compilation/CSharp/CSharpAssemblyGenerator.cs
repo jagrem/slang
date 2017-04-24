8163deb96c8f7389fbd034b92a849347a3183933
jagrem <jagremc@gmail.com>
2017-04-24 23:03:38 +1200

    Add assembly generator that uses C#
    Add an assembly generator back end that uses C# and Roslyn to create
    .NET Core assemblies. This amounts to transpiling our Slang code into
    C# and then to a dynamic assembly.

---
+++ src/Compiler/Clr/Compilation/CSharp/CSharpAssemblyGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using slang.Compiler.Clr.Compilation.Core;
+
+namespace lang.Compiler.Clr.Compilation.CSharp
+{
+    public class CSharpAssemblyGenerator : IAssemblyGenerator
+    {
+        public void GenerateDynamicAssembly(AssemblyDefinition assemblyDefinition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
