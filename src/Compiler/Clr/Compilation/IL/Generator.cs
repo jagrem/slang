961ab1d69ab2af4c8a2a563daaecfa26052487fe
jagrem <jagremc@gmail.com>
2017-04-24 20:51:17 +1200

    Stop using IL generation
    It turns out that using IL generation under .NET Core is not very easy
    at all. When I try to generate types using the assembly builder I'm not
    able to actually load the types. Dynamic assemblies cannot be saved to
    disk either which creates problems because we can't compile our Slang
    code to an executable or library.
    
    Therefore I've made the decision to remove support here for generating
    IL until such time as it is reasonable to do it on .NET Core.

--- src/Compiler/Clr/Compilation/IL/Generator.cs
+++ src/Compiler/Clr/Compilation/IL/Generator.cs
@@ -1,37 +1,13 @@
-﻿using System.Reflection;
-using System.Reflection.Emit;
+﻿using System;
 using slang.Compiler.Clr.Compilation.Definitions;
-using System;
 
 namespace slang.Compiler.Clr.Compilation.IL
 {
     public static class Generator
     {
-        public static void GenerateAssembly(AssemblyDefinition definition)
+        public static void GenerateAssembly(AssemblyDefinition assemblyDefinition)
         {
-            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly (new AssemblyName(definition.Name), AssemblyBuilderAccess.Run);
-
-            //var moduleBuilder = assemblyBuilder.DefineDynamicModule (definition.Name, definition.Filename);
-
-            //foreach (var module in definition.Modules)
-            //{
-            //    var attributes = TypeAttributes.Class | TypeAttributes.AnsiClass | TypeAttributes.AutoLayout | TypeAttributes.Abstract | TypeAttributes.Sealed | TypeAttributes.Public;
-            //    var typeBuilder = moduleBuilder.DefineType(module.Namespace + "." + module.Name, attributes);
-            //    typeBuilder.CreateType();
-
-            //    foreach (var function in module.FunctionDefinitions)
-
-            //    {
-            //        var functionVisibility = function.AccessModifier == AccessModifierType.Public ? MethodAttributes.Public : MethodAttributes.Private;
-            //        var functionBuilder = typeBuilder.DefineMethod(function.Name, functionVisibility | MethodAttributes.Static | MethodAttributes.HideBySig, typeof(int), new Type[0]);
-            //        var generator = functionBuilder.GetILGenerator();
-            //        generator.Emit(OpCodes.Ldc_I4_1);
-            //        generator.Emit(OpCodes.Ret);
-            //    }
-            //}
-
-            // NOTE: Dynamic assemblies cannot be saved to disk in .NET Core yet
-            //assemblyBuilder.Save (definition.Filename);
+            throw new NotSupportedException("IL generation is somewhat flawed on the .NET Core platform so we will avoid it for the moment.");
         }
     }
 }
