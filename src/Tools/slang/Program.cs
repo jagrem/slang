1a21774262da6af00be1379dcc205e1b3e23e356
jagrem <jagremc@gmail.com>
2018-07-14 19:27:49 +1200

    Create slang executable project
    Essentially this is a do-over of sc.exe because the idea behind the
    application has changed somewhat and it seems easier to start with the
    minimum viable product.
    
    Also 'sc' is probably too generic a name to use for your application.

---
+++ src/Tools/slang/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace slang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
