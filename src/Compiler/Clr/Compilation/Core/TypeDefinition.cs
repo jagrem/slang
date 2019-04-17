852b7e6767195828faadb8a2c1fb94999feaeacb
jagrem <jagremc@gmail.com>
2019-04-17 20:20:28 +1200

    Add note about using properties rather than functions
    While it is appealing to use functions for values on a type at this
    stage we are more likely to want to use properties.
    
    This is because a type will be a class that contains properties and may
    have various constructors.
    
    At least this is my thinking at the moment.

--- src/Compiler/Clr/Compilation/Core/TypeDefinition.cs
+++ src/Compiler/Clr/Compilation/Core/TypeDefinition.cs
@@ -7,7 +7,7 @@ namespace slang.Compiler.Clr.Compilation.Core
         public AccessModifierType AccessModifier { get; }
         public string Name { get; }
         public string Namespace { get; }
-        public IEnumerable<FunctionDefinition> FunctionDefinitions { get; }
+        public IEnumerable<FunctionDefinition> FunctionDefinitions { get; } //TODO: Change this to a collection of properties
 
         public TypeDefinition(AccessModifierType accessModifier, string name, string @namespace, IEnumerable<FunctionDefinition> functionDefinitions)
         {
